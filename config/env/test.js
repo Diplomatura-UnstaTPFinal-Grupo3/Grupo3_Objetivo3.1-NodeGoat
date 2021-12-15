a6a7b8106ee46730901454eea3edb171d2eb092c
NahuelGiliberto <87381659+NahuelGiliberto@users.noreply.github.com>
2021-12-15 09:39:49 -0300

    Add files via upload

---
+++ config/env/test.js
@@ -0,0 +1,8 @@
+module.exports = {
+   // If you want to debug regression tests, you will need the following.
+   zapHostName: "localhost",
+   zapPort: "8080",
+   // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
+   zapApiKey: "6q2tfp7icl1gbh9jteafqcnrt",
+   zapApiFeedbackSpeed: 5000 // Milliseconds.
+};
